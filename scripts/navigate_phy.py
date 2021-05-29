fae2f0e56426760502f2f56af492966b0fadf2d1
Dewey <deweyfang@gmail.com>
2021-05-29 14:40:52 -0700

    updated phy nav using box pose estimate

--- scripts/navigate_phy.py
+++ scripts/navigate_phy.py
@@ -36,7 +36,9 @@ import trajectory_msgs.msg
 
 import struct
 
-from geometry_msgs.msg import Point, PointStamped
+from geometry_msgs.msg import Point, PointStamped, PoseStamped
+
+from robotics_labs.msg import BoxTarget
 
 import tf2_ros
 import tf2_geometry_msgs
@@ -61,11 +63,13 @@ porpotion_tilt_prev = 0
 intergral_tilt = 0
 traj_client = None
 current_dist = 0
-cx = 0
-cy = 0
+cx = None
+cy = None
 image_sub = None
 dist_sub = None
 tilt = 0
+tf_buffer = None
+stop = False
 
 def dist_callback(msg):
     global current_dist
@@ -73,22 +77,21 @@ def dist_callback(msg):
     global cy
     global tilt
     global dist_sub
-    array_position = int(cy)*msg.row_step + int(cx)*msg.point_step
-    camPoint = Point()
-    (camPoint.x, camPoint.y, camPoint.z) = struct.unpack_from('fff', msg.data, offset=array_position)
+    global stop
 
-    current_dist = camPoint.z
-    if (not math.isnan(current_dist)) and current_dist < 0.9 and current_dist != 0:
-        dist_sub.unregister()
-        tf_buffer = tf2_ros.Buffer(rospy.Duration(1)) #tf buffer length
-        tf_listener = tf2_ros.TransformListener(tf_buffer)
-        transform = tf_buffer.lookup_transform("base_link",
+    transform = tf_buffer.lookup_transform("base_link",
                                             "head_camera_rgb_optical_frame", #source frame
                                             rospy.Time(0),
                                             rospy.Duration(5.0)) #get the tf at first available time
-        point_wrt_target = tf2_geometry_msgs.do_transform_point(PointStamped(point=camPoint),
-            transform).point
-        print point_wrt_target.x
+    pose_goal = PoseStamped()
+    pose_goal.header.frame_id = "head_camera_rgb_optical_frame"
+    pose_goal.pose = msg.box_pose
+    pose_transformed = tf2_geometry_msgs.do_transform_pose(pose_goal, transform)
+
+    current_dist = pose_transformed.pose.position.x
+
+    if (not math.isnan(current_dist)) and current_dist < 0.82 and current_dist != 0:
+        stop = True
 
     print "current_dist = ", current_dist
 
@@ -107,9 +110,9 @@ def image_callback(msg):
     global image_sub
     global tilt
     # print("Received an image!")
-    if (not math.isnan(current_dist)) and current_dist < 0.9 and current_dist != 0:
-        fetch_base.move(0, 0)
-        image_sub.unregister()
+    # if (not math.isnan(current_dist)) and current_dist < 0.85 and current_dist != 0:
+    #     fetch_base.move(0, 0)
+    #     image_sub.unregister()
     try:
         # Convert your ROS Image message to OpenCV2
         cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
@@ -167,14 +170,22 @@ def main():
     global traj_client
     global image_sub
     global dist_sub
+    global tf_buffer
     rospy.init_node('dewey_fetch_nav')
+    tf_buffer = tf2_ros.Buffer(rospy.Duration(1))
+    tf2_ros.TransformListener(tf_buffer)
     traj_client = actionlib.SimpleActionClient('head_controller/follow_joint_trajectory', control_msgs.msg.FollowJointTrajectoryAction)
     # Define your image topic
     image_topic = "/head_camera/rgb/image_raw"
     # Set up your subscriber and define its callback
     image_sub = rospy.Subscriber(image_topic, Image, image_callback)
-    dist_sub = rospy.Subscriber("/head_camera/depth_registered/points", PointCloud2, dist_callback)
+    dist_sub = rospy.Subscriber("box_target", BoxTarget, dist_callback)
     # Spin until ctrl + c. You can also choose to spin once
+    while not rospy.is_shutdown():
+        if stop:
+            image_sub.unregister()
+            dist_sub.unregister()
+        rospy.sleep(0.1)
     rospy.spin()
 
 if __name__ == '__main__':
